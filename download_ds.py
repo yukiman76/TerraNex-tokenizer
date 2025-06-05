8c258d1bbd47905ef092cd3d7befb87927405aac
jeff <jeffreyr@securitydept.com>
2025-06-05 14:14:11 -0400

    recovery

--- download_ds.py
+++ download_ds.py
@@ -3,10 +3,10 @@ from datasets import load_dataset, DownloadConfig
 download_config = DownloadConfig(resume_download=True, max_retries=3)
 
 data_sets = [
-    "bigcode/the-stack-march-sample-special-tokens-stripped",  # 1.1G
-    "codeparrot/github-code",  # 1.1 TB
-    "bigcode/the-stack-github-issues",  # 66.6 G
-    "iohadrubin/wikitext-103-raw-v1",  # 310M
+    # "bigcode/the-stack-march-sample-special-tokens-stripped",  # 1.1G
+    # "codeparrot/github-code",  # 1.1 TB
+    # "bigcode/the-stack-github-issues",  # 66.6 G
+    # "iohadrubin/wikitext-103-raw-v1",  # 310M
 ]
 
 # download entire DS
@@ -23,26 +23,33 @@ langs = ['swe_Latn', 'eng_Latn', 'spa_Latn', 'deu_Latn', 'cym_Latn', 'dan_Latn',
 
 # "oscar-corpus/mOSCAR" trust_remote_cod# #190G
 for lang in langs:
-    print(f"oscar-corpus/mOSCAR  {lang}")
-    _ = load_dataset(
-       "oscar-corpus/mOSCAR",
-        lang,
-        trust_remote_code=True,
-        download_config=download_config,
-        num_proc=1,
-        data_dir="./datasets"
-    )
+    try:
+        print(f"oscar-corpus/mOSCAR  {lang}")
+        _ = load_dataset(
+        "oscar-corpus/mOSCAR",
+            lang,
+            trust_remote_code=True,
+            download_config=download_config,
+            num_proc=1,
+            data_dir="./datasets"
+        )
+    except Exception as e:
+        print(e)
+
 
 langs = ["sv", "en", "es", "de", "cy", "da", "fr", "it", "la", "nl", "no"]
 
 # "statmt/cc100" trust_remote_cod# 200G
 for lang in langs:
     print(f"statmt/cc100  {lang}")
-    _ = load_dataset(
-        "statmt/cc100",
-        name=lang,
-        trust_remote_code=True,
-        download_config=download_config,
-        num_proc=1,
-        data_dir="./datasets"
-    )
+    try:
+        _ = load_dataset(
+            "statmt/cc100",
+            name=lang,
+            trust_remote_code=True,
+            download_config=download_config,
+            num_proc=1,
+            data_dir="./datasets"
+        )
+    except Exception as e:
+        print(e)
