60f45fb4b0887374dc5644a0a1de944a2b16a931
jeff <jeffreyr@securitydept.com>
2025-06-17 09:30:57 -0400

    combining into one script

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -e
 export TOKENIZERS_PARALLELISM=True
 
-python train_tokenizer_v2.py --download-only
+# python train_tokenizer_v2.py --download-only
 python train_tokenizer_v2.py --max_workers 20 --offline 
