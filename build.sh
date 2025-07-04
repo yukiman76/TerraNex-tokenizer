cf34b6175c96ca0dee51f0fc7c9bf556dd92abb5
jeff <jeffreyr@securitydept.com>
2025-07-04 10:34:13 -0400

    up the tok vocab size

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -e
 export TOKENIZERS_PARALLELISM=True
 
-python train_tokenizer_v3.py --max_workers 1 --offline --streaming
+python train_tokenizer_v3.py --max_workers 1 --offline --streaming --vocab-size 256000
