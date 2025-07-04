5ad1a5a04d64f65cddd300e9f1909c95339ebaf4
jeff <jeffreyr@securitydept.com>
2025-07-04 10:39:51 -0400

    up the tok vocab size

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 set -e
 export TOKENIZERS_PARALLELISM=True
 
+# we hould set the vocab size to around 400k for prod but not to exceed 500k
 python train_tokenizer_v3.py --max_workers 1 --offline --streaming --vocab-size 256000
