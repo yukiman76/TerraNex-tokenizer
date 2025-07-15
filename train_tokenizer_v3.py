f21257f74ac6d09457759641de51d326b422765e
jeff <jeffreyr@securitydept.com>
2025-07-15 07:57:20 -0400

    remove sample_size

--- train_tokenizer_v3.py
+++ train_tokenizer_v3.py
@@ -457,7 +457,7 @@ def train_tokenizer(
         my_datasets = load_all_datasets(
             max_workers=max_workers,
             streaming=streaming,
-            sample_size=1_500_000,
+            # sample_size=1_500_000,
             offline_mode=offline_mode,
             local_data_dir=local_data_dir,
         )
